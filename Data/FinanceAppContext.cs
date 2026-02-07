508f72327e5cf8172834b504c49914f9f8329030
Karol_Czernecki <inz.karol.czernecki@gmail.com>
2026-02-07 11:46:08 +0100

    Dodaj pliki projektów.

---
+++ Data/FinanceAppContext.cs
@@ -0,0 +1,13 @@
+﻿using FinanceAPP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceAPP.Data
+{
+    public class FinanceAppContext : DbContext 
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options):base(options) { }  //What this line does?
+
+
+        DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ Models/Expense.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceAPP.Models
+{
+    public class Expense
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        public double Amount { get; set; }
+        [Required]
+        [Range(0.01 , double.MaxValue,ErrorMessage = "Amount should be higher than 0 and lower than double.MaxValue!")]
+        public string Category { get; set; } = null!;
+        [Required]
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
